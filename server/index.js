aa2e90f8fe6a961fe3a381a6b6ff6351aa55a803
AVULA SIVA SANKAR <116957401+sivasankar128@users.noreply.github.com>
2025-07-01 12:44:17 +0530

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: { origin: '*' }
+});
+
+app.use(cors());
+
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  socket.on('join-room', (roomId) => {
+    socket.join(roomId);
+    console.log(`User ${socket.id} joined room: ${roomId}`);
+  });
+
+  socket.on('drawing', ({ roomId, data }) => {
+    socket.to(roomId).emit('drawing', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+const PORT = 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
